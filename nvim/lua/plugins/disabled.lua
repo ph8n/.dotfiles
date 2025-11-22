e63ce66a54452115b769ea051020dde8798b8afa
Phong Do <Donguyenphong@icloud.com>
2025-11-22 13:21:49 -0800

    update dotfiles: ghostty, karabiner, nvim, tmux
    Updated configurations for Ghostty, Karabiner, Neovim (LazyVim), and Tmux.
    
    Removed untracked backups and lock files.

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "Saghen/blink.cmp", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "LunarVim/grug-far.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
