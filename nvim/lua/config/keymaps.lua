ac7900d996212b6fa90cc31384385d93f3fabac1
Phong Do <Donguyenphong@icloud.com>
2025-12-07 00:51:15 -0800

    added oil.nvim plugin for file explorer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 local map = vim.keymap.set
 
+map("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
 -- Better up/down on wrapped lines
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
