8541755aa11ed0a229374caa4eb8a187d39bfe01
Phong Do <Donguyenphong@icloud.com>
2025-12-09 02:45:04 -0800

    added trouble.nvim>

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -44,15 +44,15 @@ return {
     { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
 
     -- Diagnostics
-    { "<leader>xx", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
 
     -- LSP
     { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
     { "gr", function() Snacks.picker.lsp_references() end, desc = "References" },
     { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
     { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
-    { "<leader>cs", function() Snacks.picker.lsp_symbols() end, desc = "Document Symbols" },
-    { "<leader>cS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "Workspace Symbols" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "Document Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "Workspace Symbols" },
     { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
 
     -- Buffer
