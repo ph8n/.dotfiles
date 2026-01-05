96f173ffdfb00701377189d9845909fa7ac3d353
Phong Do <Donguyenphong@icloud.com>
2026-01-04 19:50:24 -0800

    2 space/tab

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,8 +9,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Tabs & Indentation
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
