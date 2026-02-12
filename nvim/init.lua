9472c06119c5644eec5e43102e41fdeb2bdcc1aa
Phong Do <donguyenphong@icloud.com>
2026-02-12 15:59:29 -0800

    Added autocomp (blink)

--- nvim/init.lua
+++ nvim/init.lua
@@ -237,7 +237,29 @@ require("lazy").setup({
     end,
   },
   {
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        list = {
+          selection = { preselect = false, auto_insert = true },
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    },
+    opts_extend = { "sources.default" },
+  },
+  {
     "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
     config = function()
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(ev)
