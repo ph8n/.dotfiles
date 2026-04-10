9e0d2a23da7b722fdc5d073ac7cc6d017e8e9f66
Phong Do <donguyenphong@icloud.com>
2026-04-10 00:42:19 -0700

    ful cchud

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -40,14 +40,14 @@ config.send_composed_key_when_right_alt_is_pressed = false
 config.window_background_opacity = 1.0
 config.macos_window_background_blur = 20
 config.default_cursor_style = 'SteadyBlock'
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.window_decorations = 'RESIZE'
 config.show_tabs_in_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
 config.show_new_tab_button_in_tab_bar = false
 config.window_padding = {
-  left = 20,
-  right = 20,
+  left = 25,
+  right = 25,
   top = 20,
   bottom = 20,
 }
@@ -62,44 +62,8 @@ local rename_tab = act.PromptInputLine {
   end),
 }
 
-local function pane_nav(direction)
-  local key_by_direction = {
-    Left = 'h',
-    Down = 'j',
-    Up = 'k',
-    Right = 'l',
-  }
-
-  return wezterm.action_callback(function(window, pane)
-    local process = pane:get_foreground_process_name()
-    if process and process:match('tmux$') then
-      window:perform_action(
-        act.SendKey { key = key_by_direction[direction], mods = 'CTRL' },
-        pane
-      )
-      return
-    end
-
-    window:perform_action(act.ActivatePaneDirection(direction), pane)
-  end)
-end
-
 config.keys = {
-  { key = 'd', mods = 'CMD', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'd', mods = 'CMD|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
   { key = 'y', mods = 'CMD', action = act.ActivateCopyMode },
-  { key = 'h', mods = 'CTRL', action = pane_nav 'Left' },
-  { key = 'j', mods = 'CTRL', action = pane_nav 'Down' },
-  { key = 'k', mods = 'CTRL', action = pane_nav 'Up' },
-  { key = 'l', mods = 'CTRL', action = pane_nav 'Right' },
-  { key = 'h', mods = 'CMD|ALT', action = act.ActivatePaneDirection 'Left' },
-  { key = 'j', mods = 'CMD|ALT', action = act.ActivatePaneDirection 'Down' },
-  { key = 'k', mods = 'CMD|ALT', action = act.ActivatePaneDirection 'Up' },
-  { key = 'l', mods = 'CMD|ALT', action = act.ActivatePaneDirection 'Right' },
-  { key = 'h', mods = 'CMD|ALT|SHIFT', action = act.AdjustPaneSize { 'Left', 5 } },
-  { key = 'j', mods = 'CMD|ALT|SHIFT', action = act.AdjustPaneSize { 'Down', 5 } },
-  { key = 'k', mods = 'CMD|ALT|SHIFT', action = act.AdjustPaneSize { 'Up', 5 } },
-  { key = 'l', mods = 'CMD|ALT|SHIFT', action = act.AdjustPaneSize { 'Right', 5 } },
   { key = 'h', mods = 'CMD|SHIFT', action = act.ActivateTabRelative(-1) },
   { key = 'l', mods = 'CMD|SHIFT', action = act.ActivateTabRelative(1) },
   { key = 'r', mods = 'CMD', action = rename_tab },
