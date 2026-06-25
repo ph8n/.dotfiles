bd82c36fefb78240db71a6dfbc99cb9cfcfe7ad1
phongndo <donguyenphong@icloud.com>
2026-06-24 23:38:58 -0700

    Update dotfiles config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,114 +1,178 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.o.termguicolors = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.cursorline = true
-vim.o.wrap = false
-vim.o.scrolloff = 8
-vim.o.sidescrolloff = 8
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-vim.o.lazyredraw = true
-vim.o.synmaxcol = 240
-vim.o.clipboard = "unnamedplus"
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
-vim.o.smarttab = true
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
-vim.o.hlsearch = false
-vim.o.swapfile = false
-vim.o.backup = false
-vim.o.undofile = true
-vim.o.splitbelow = true
-vim.o.splitright = true
-vim.o.completeopt = "menu,menuone,noselect,popup,fuzzy"
-vim.o.complete = ".,w,b,u,t,o"
-vim.o.autocomplete = true
-vim.o.redrawtime = 1500
-vim.o.list = true
-vim.o.listchars = "tab:▸ ,trail:·,nbsp:␣"
+for key, value in pairs({
+  termguicolors = true,
+  number = true,
+  relativenumber = true,
+  signcolumn = "yes",
+  cursorline = true,
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  updatetime = 250,
+  timeoutlen = 300,
+  clipboard = "unnamedplus",
+  tabstop = 2,
+  shiftwidth = 2,
+  softtabstop = 2,
+  expandtab = true,
+  smarttab = true,
+  autoindent = true,
+  ignorecase = true,
+  smartcase = true,
+  incsearch = true,
+  hlsearch = false,
+  swapfile = false,
+  backup = false,
+  undofile = true,
+  splitbelow = true,
+  splitright = true,
+  completeopt = "menu,menuone,noselect",
+  autocomplete = false,
+  list = true,
+  listchars = "tab:▸ ,trail:·,nbsp:␣",
+}) do
+  vim.o[key] = value
+end
 
 local map = vim.keymap.set
-local augroup = vim.api.nvim_create_augroup
 
 local function toggle_quickfix()
-  local quickfix = vim.fn.getqflist({ winid = 0 })
-  if quickfix.winid ~= 0 then
+  if vim.fn.getqflist({ winid = 0 }).winid ~= 0 then
     vim.cmd.cclose()
   else
     vim.cmd.copen()
   end
 end
 
-map("v", "<", "<gv", { silent = true, desc = "Indent left and keep selection" })
-map("v", ">", ">gv", { silent = true, desc = "Indent right and keep selection" })
-map("n", "<leader>h", vim.cmd.nohlsearch, { desc = "Clear search highlight" })
-map("n", "<leader>uw", "<cmd>set wrap!<CR>", { desc = "Toggle line wrap" })
-map("n", "<leader>ul", "<cmd>set number!<CR>", { desc = "Toggle line numbers" })
-map("n", "<leader>uL", "<cmd>set relativenumber!<CR>", { desc = "Toggle relative line numbers" })
-map("n", "<leader>us", "<cmd>set spell!<CR>", { desc = "Toggle spelling" })
-map("n", "<leader>m", "<cmd>make<CR>", { desc = "Run make" })
-map("n", "<leader>.", toggle_quickfix, { desc = "Toggle quickfix window" })
-map("n", "]q", "<cmd>cnext<CR>", { desc = "Quickfix next item" })
-map("n", "[q", "<cmd>cprev<CR>", { desc = "Quickfix previous item" })
-map("n", "<leader>v", vim.cmd.vsplit, { desc = "Vertical split" })
-map("n", "<leader>s", vim.cmd.split, { desc = "Horizontal split" })
-map("n", "<leader>x", vim.cmd.close, { desc = "Close split" })
-map("v", "J", ":m '>+1<CR>gv=gv", { silent = true, desc = "Move selection down" })
-map("v", "K", ":m '<-2<CR>gv=gv", { silent = true, desc = "Move selection up" })
+map("v", "<", "<gv", { silent = true, desc = "Indent left" })
+map("v", ">", ">gv", { silent = true, desc = "Indent right" })
+map("n", "<leader>h", vim.cmd.nohlsearch, { desc = "Clear search" })
+map("n", "<leader>uw", "<cmd>set wrap!<cr>", { desc = "Toggle wrap" })
+map("n", "<leader>ul", "<cmd>set number!<cr>", { desc = "Toggle number" })
+map("n", "<leader>uL", "<cmd>set relativenumber!<cr>", { desc = "Toggle relativenumber" })
+map("n", "<leader>us", "<cmd>set spell!<cr>", { desc = "Toggle spell" })
+map("n", "<leader>m", "<cmd>make<cr>", { desc = "Make" })
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>.", toggle_quickfix, { desc = "Toggle quickfix" })
+map("n", "]q", "<cmd>cnext<cr>", { desc = "Quickfix next" })
+map("n", "[q", "<cmd>cprev<cr>", { desc = "Quickfix previous" })
+map("n", "]d", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Diagnostic next" })
+map("n", "[d", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Diagnostic previous" })
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostic float" })
+map("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Diagnostics to quickfix" })
+map("n", "<leader>v", "<cmd>vsplit<cr>", { desc = "Vertical split" })
+map("n", "<leader>s", "<cmd>split<cr>", { desc = "Horizontal split" })
+map("n", "<leader>x", "<cmd>close<cr>", { desc = "Close split" })
+map("v", "J", ":m '>+1<cr>gv=gv", { silent = true, desc = "Move selection down" })
+map("v", "K", ":m '<-2<cr>gv=gv", { silent = true, desc = "Move selection up" })
 map("x", "<leader>p", '"_dP', { desc = "Paste without yanking replaced text" })
-map("i", "<CR>", function()
-  if vim.fn.pumvisible() == 1 and vim.fn.complete_info({ "selected" }).selected ~= -1 then
-    return "<C-y>"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+  if vim.v.shell_error ~= 0 then
+    error("lazy.nvim install failed:\n" .. out)
   end
-
-  return "<CR>"
-end, { expr = true, desc = "Accept completion or insert newline" })
-
-local function gh(repo)
-  return "https://github.com/" .. repo
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.pack.add({
-  { src = gh("projekt0n/github-nvim-theme"), name = "github-theme" },
-  gh("nvim-treesitter/nvim-treesitter"),
-  gh("ibhagwan/fzf-lua"),
-  gh("nvim-tree/nvim-web-devicons"),
-  gh("stevearc/oil.nvim"),
-  gh("lewis6991/gitsigns.nvim"),
-}, { confirm = false })
-
-require("github-theme").setup({
-  options = {
-    transparent = true,
-    terminal_colors = true,
-    dim_inactive = false,
-    styles = {
-      comments = "italic",
+require("lazy").setup({
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      options = {
+        transparent = true,
+        terminal_colors = true,
+        styles = { comments = "italic" },
+      },
+    },
+    config = function(_, opts)
+      require("github-theme").setup(opts)
+      vim.cmd.colorscheme("github_dark_high_contrast")
+    end,
+  },
+  { "neovim/nvim-lspconfig", lazy = false },
+  {
+    "ibhagwan/fzf-lua",
+    cmd = "FzfLua",
+    keys = {
+      { "<leader>f", "<cmd>FzfLua files<cr>", desc = "Find files" },
+      { "<leader>/", "<cmd>FzfLua live_grep<cr>", desc = "Live grep" },
+      { "<leader>,", "<cmd>FzfLua buffers<cr>", desc = "Buffers" },
+    },
+    opts = {
+      winopts = {
+        height = 0.85,
+        width = 0.85,
+        preview = { layout = "horizontal" },
+      },
     },
   },
-  groups = {
-    github_dark_high_contrast = {
-      Pmenu = { bg = "#151515" },
-      NormalFloat = { bg = "#151515" },
-      FloatBorder = { bg = "#151515" },
+  { "stevearc/oil.nvim", cmd = "Oil", keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent" } }, opts = {} },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+      update_debounce = 200,
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    version = "*",
+    event = "InsertEnter",
+    opts = {
+      keymap = { preset = "enter" },
+      completion = {
+        list = { selection = { preselect = false, auto_insert = false } },
+      },
+      cmdline = { enabled = false },
+      sources = { default = { "lsp", "path", "buffer" } },
+    },
+  },
+}, {
+  defaults = { lazy = true },
+  checker = { enabled = false },
+  change_detection = { enabled = false, notify = false },
+  install = { colorscheme = { "github_dark_high_contrast" } },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
   },
 })
-
-vim.cmd.colorscheme("github_dark_high_contrast")
 
 vim.api.nvim_set_hl(0, "StatusLine", { fg = "#e6edf3", bg = "#2d333b", bold = false })
 vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#9da7b3", bg = "#22272e", bold = false })
@@ -172,74 +236,60 @@ vim.o.statusline = table.concat({
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("axiom_treesitter", { clear = true }),
+  group = vim.api.nvim_create_augroup("treesitter_start", { clear = true }),
   callback = function(args)
     pcall(vim.treesitter.start, args.buf)
   end,
 })
 
--- nvim-web-devicons loaded lazily with fzf-lua or oil.nvim
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+vim.lsp.config("*", { capabilities = capabilities })
 
-local devicons_loaded = false
-local function ensure_devicons()
-  if not devicons_loaded then
-    require("nvim-web-devicons").setup({ default = true })
-    devicons_loaded = true
-  end
-end
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp_keymaps", { clear = true }),
+  callback = function(args)
+    local opts = function(desc)
+      return { buffer = args.buf, desc = desc }
+    end
 
-local fzf_loaded = false
-local function load_fzf()
-  ensure_devicons()
-  if not fzf_loaded then
-    local fzf = require("fzf-lua")
-    fzf.setup({
-      winopts = {
-        height = 0.85,
-        width = 0.85,
-        preview = {
-          layout = "horizontal",
-          vertical = "down:50%",
-        },
-      },
-    })
-    fzf_loaded = true
-  end
-  return require("fzf-lua")
-end
-
-map("n", "<leader>f", function() load_fzf().files() end, { desc = "Find files" })
-map("n", "<leader>/", function() load_fzf().live_grep() end, { desc = "Live grep" })
-map("n", "<leader>,", function() load_fzf().buffers() end, { desc = "List buffers" })
-
-local oil_loaded = false
-map("n", "-", function()
-  if not oil_loaded then
-    ensure_devicons()
-    require("oil").setup()
-    oil_loaded = true
-  end
-  require("oil").open()
-end, { desc = "Open parent directory" })
-
-require("gitsigns").setup({
-  signs = {
-    add = { text = "+" },
-    change = { text = "~" },
-    delete = { text = "_" },
-    topdelete = { text = "‾" },
-    changedelete = { text = "~" },
-  },
-  update_debounce = 200,
-  preview_config = {
-    border = "single",
-    row = 1,
-  },
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-
-    map("n", "]h", gs.next_hunk, { buffer = bufnr, desc = "Next git hunk" })
-    map("n", "[h", gs.prev_hunk, { buffer = bufnr, desc = "Previous git hunk" })
-    map("n", "<leader>g", gs.preview_hunk, { buffer = bufnr, desc = "Preview git hunk" })
+    map("n", "gd", vim.lsp.buf.definition, opts("LSP definition"))
+    map("n", "gD", vim.lsp.buf.declaration, opts("LSP declaration"))
+    map("n", "gi", vim.lsp.buf.implementation, opts("LSP implementation"))
+    map("n", "gr", vim.lsp.buf.references, opts("LSP references"))
+    map("n", "gy", vim.lsp.buf.type_definition, opts("LSP type definition"))
+    map("n", "K", vim.lsp.buf.hover, opts("LSP hover"))
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts("LSP rename"))
+    map("n", "<leader>ca", vim.lsp.buf.code_action, opts("LSP code action"))
+    map("n", "<leader>cf", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts("LSP format"))
+    map("n", "<leader>ds", vim.lsp.buf.document_symbol, opts("LSP document symbols"))
+    map("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts("LSP workspace symbols"))
   end,
 })
+
+local servers = {}
+local function add_lsp(name, cmd)
+  if vim.fn.executable(cmd) == 1 then
+    servers[#servers + 1] = name
+    return true
+  end
+  return false
+end
+
+add_lsp("clangd", "clangd")
+add_lsp("rust_analyzer", "rust-analyzer")
+if not add_lsp("basedpyright", "basedpyright-langserver") and not add_lsp("pyright", "pyright-langserver") then
+  add_lsp("ruff", "ruff")
+end
+add_lsp("ts_ls", "typescript-language-server")
+add_lsp("zls", "zls")
+if not add_lsp("texlab", "texlab") then
+  add_lsp("digestif", "digestif")
+end
+if not add_lsp("marksman", "marksman") then
+  add_lsp("markdown_oxide", "markdown-oxide")
+end
+
+vim.lsp.enable(servers)
