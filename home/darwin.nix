e9cfb2b8131d55a519d5a4a0ae83ee762437aae3
phongndo <donguyenphong@icloud.com>
2026-08-24 00:05:34 -0700

    Use the Tailscale MagicDNS name for box SSH

--- home/darwin.nix
+++ home/darwin.nix
@@ -47,7 +47,7 @@ in
     enableDefaultConfig = false;
     settings = {
       box = {
-        HostName = "box.tail5606b4.ts.net";
+        HostName = "nixos";
         User = "box";
         ServerAliveInterval = 30;
         ServerAliveCountMax = 3;
