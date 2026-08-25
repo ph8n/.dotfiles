4f62d744a06ec0218ec840ee0159a1f99809136f
phongndo <donguyenphong@icloud.com>
2026-08-24 20:23:11 -0700

    migrate box to Determinate Nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    determinate-nix = {
+      url = "https://flakehub.com/f/DeterminateSystems/nix-src/3.22.2";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
@@ -21,6 +25,7 @@
       self,
       nixpkgs,
       nixpkgs-unstable,
+      determinate-nix,
       home-manager,
       nix-darwin,
       ...
@@ -48,6 +53,17 @@
         name = "box";
         system = linuxSystem;
         user = "z";
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              # Keep Nix managed by NixOS while replacing only its package with
+              # the pinned Determinate distribution.
+              nix.package = pkgs.nix;
+              nixpkgs.overlays = [ determinate-nix.overlays.default ];
+            }
+          )
+        ];
       };
 
       qualityChecks =
